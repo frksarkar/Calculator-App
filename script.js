9dd3bff67837676fa650ea5ce5210871380f34cf
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-04-30 20:24:24 +0600

    add input validate function

--- script.js
+++ script.js
@@ -4,86 +4,107 @@ const displayOutput = document.querySelector('.display .output');
 
 let input = '';
 
-for(let key of keys){
-    const value = key.dataset.key
-    key.addEventListener('click', ()=> {
-        if(value == 'clear'){
+for (let key of keys) {
+	const value = key.dataset.key;
+	key.addEventListener('click', () => {
+		if (value == 'clear') {
+			input = '';
+			displayInput.innerHTML = '';
+			displayOutput.innerHTML = '';
+		} else if (value == 'backspace') {
+			input = input.slice(0, -1);
+			displayInput.innerHTML = cleanInput(input);
+		} else if (value == '=') {
+			let result = eval(input);
 
-            input = '';
-            displayInput.innerHTML = '';
-            displayOutput.innerHTML = '';
-        } else if (value == 'backspace') {
-            input = input.slice(0, -1);
-            displayInput.innerHTML = cleanInput(input);
-        } else if (value == '=') {
-            let result = eval(input);
-
-            displayOutput.innerHTML = cleanOutput(result);
-        } else if (value == 'brackets') {
-            if(input.indexOf('(') == -1 ||
-                input.indexOf('(') != -1 &&
-                input.indexOf(')') != -1 &&
-                input.lastIndexOf('(') < input.lastIndexOf(')')
-            ){
-                input += '(';
-            } else if (
-                input.indexOf('(' != -1 &&
-                input.indexOf(')') == -1 ||
-                input.indexOf('(') != -1 &&
-                input.indexOf(')') != -1 &&
-                input.lastIndexOf('(') > input.lastIndexOf(')'))
-            ){
-                input += ')';
-            }
-            displayInput.innerHTML = cleanInput(input);
-        } else {
-            input += value;
-            displayInput.innerHTML = cleanInput(input);
-        }
-    })
+			displayOutput.innerHTML = cleanOutput(result);
+		} else if (value == 'brackets') {
+			if (
+				input.indexOf('(') == -1 ||
+				(input.indexOf('(') != -1 &&
+					input.indexOf(')') != -1 &&
+					input.lastIndexOf('(') < input.lastIndexOf(')'))
+			) {
+				input += '(';
+			} else if (
+				input.indexOf(
+					('(' != -1 && input.indexOf(')') == -1) ||
+						(input.indexOf('(') != -1 &&
+							input.indexOf(')') != -1 &&
+							input.lastIndexOf('(') > input.lastIndexOf(')'))
+				)
+			) {
+				input += ')';
+			}
+			displayInput.innerHTML = cleanInput(input);
+		} else {
+			if (validateInput(value)) {
+				input += value;
+				displayInput.innerHTML = cleanInput(input);
+			}
+		}
+	});
 }
 
-function cleanInput(input){
-    const inputArray = input.split('');
-    const inputArrayLength = inputArray.length;
+function cleanInput(input) {
+	const inputArray = input.split('');
+	const inputArrayLength = inputArray.length;
 
-    for (let i = 0; i < inputArrayLength; i++) {
-        if(inputArray[i] == '*'){
-            inputArray[i] = ` <span class='operator'>x</span> `
-        } else  if(inputArray[i] == '/'){
-            inputArray[i] = ` <span class='operator'>÷</span> `
-        }else  if(inputArray[i] == '+'){
-            inputArray[i] = ` <span class='operator'>+</span> `
-        }else  if(inputArray[i] == '-'){
-            inputArray[i] = ` <span class='operator'>-</span> `
-        } else  if(inputArray[i] == '%'){
-            inputArray[i] = ` <span class='operator'>%</span> `
-        } else  if(inputArray[i] == '('){
-            inputArray[i] = `<span class='operator'>(</span>`
-        } else  if(inputArray[i] == ')'){
-            inputArray[i] = `<span class='operator'>)</span>`
-        }
-    }
-    return inputArray.join('');
+	for (let i = 0; i < inputArrayLength; i++) {
+		if (inputArray[i] == '*') {
+			inputArray[i] = ` <span class='operator'>x</span> `;
+		} else if (inputArray[i] == '/') {
+			inputArray[i] = ` <span class='operator'>÷</span> `;
+		} else if (inputArray[i] == '+') {
+			inputArray[i] = ` <span class='operator'>+</span> `;
+		} else if (inputArray[i] == '-') {
+			inputArray[i] = ` <span class='operator'>-</span> `;
+		} else if (inputArray[i] == '%') {
+			inputArray[i] = ` <span class='operator'>%</span> `;
+		} else if (inputArray[i] == '(') {
+			inputArray[i] = `<span class='operator'>(</span>`;
+		} else if (inputArray[i] == ')') {
+			inputArray[i] = `<span class='operator'>)</span>`;
+		}
+	}
+	return inputArray.join('');
 }
 
-function cleanOutput(output){
-    let outputString = output.toString();
-    let decimal = outputString.split('.')[1];
-    outputString = outputString.split('.')[0];
+function cleanOutput(output) {
+	let outputString = output.toString();
+	let decimal = outputString.split('.')[1];
+	outputString = outputString.split('.')[0];
 
-    let outputArray = outputString.split('');
+	let outputArray = outputString.split('');
 
-    if(outputArray.length > 3) {
-        for (let i = outputArray.length - 3; i > 0; i -= 3){
-            outputArray.splice(i, 0, ',');
-        }
-    }
+	if (outputArray.length > 3) {
+		for (let i = outputArray.length - 3; i > 0; i -= 3) {
+			outputArray.splice(i, 0, ',');
+		}
+	}
 
-    if(decimal) {
-        outputArray.push('.');
-        outputArray.push(decimal);
-    }
+	if (decimal) {
+		outputArray.push('.');
+		outputArray.push(decimal);
+	}
 
-    return outputArray.join('');
+	return outputArray.join('');
+}
+
+function validateInput(value) {
+	let lastInput = input.slice(-1);
+	const operators = ['+', '-', '*', '/'];
+
+	if (value == '.' && lastInput == '.') {
+		return false;
+	}
+	if (operators.includes(value)) {
+		if (operators.includes(lastInput)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	return true;
 }
