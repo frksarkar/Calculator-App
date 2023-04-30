c94563ac1ea2c4178db9fc03c5bc57d9d03f3d1c
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-04-30 16:45:02 +0600

    add clean output function

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ for(let key of keys){
         } else if (value == '=') {
             let result = eval(input);
 
-            displayOutput.innerHTML = result;
+            displayOutput.innerHTML = cleanOutput(result);
         } else if (value == 'brackets') {
             if(input.indexOf('(') == -1 ||
                 input.indexOf('(') != -1 &&
@@ -65,4 +65,25 @@ function cleanInput(input){
         }
     }
     return inputArray.join('');
+}
+
+function cleanOutput(output){
+    let outputString = output.toString();
+    let decimal = outputString.split('.')[1];
+    outputString = outputString.split('.')[0];
+
+    let outputArray = outputString.split('');
+
+    if(outputArray.length > 3) {
+        for (let i = outputArray.length - 3; i > 0; i -= 3){
+            outputArray.splice(i, 0, ',');
+        }
+    }
+
+    if(decimal) {
+        outputArray.push('.');
+        outputArray.push(decimal);
+    }
+
+    return outputArray.join('');
 }
